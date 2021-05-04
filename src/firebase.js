1674557971a58e85d480ba78a82b0c4713c304e1
Mohammad Bilal <mohdbilal16@gmail.com>
2021-05-04 15:42:58 +0530

    added cofig

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,14 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDp49Emi3_vpkn3yNBd0ajCI6hbAVVERfc",
-    authDomain: "chatter-76627.firebaseapp.com",
-    projectId: "chatter-76627",
-    storageBucket: "chatter-76627.appspot.com",
-    messagingSenderId: "556644794663",
-    appId: "1:556644794663:web:760a44958a5312ed046902",
-    measurementId: "G-TQL8DPL0F7"
+    apiKey: "AIzaSyCN3Uz1OLZLMy3DODJazNf7BIg2rQirQmI",
+    authDomain: "voidsapp.firebaseapp.com",
+    projectId: "voidsapp",
+    storageBucket: "voidsapp.appspot.com",
+    messagingSenderId: "512415915277",
+    appId: "1:512415915277:web:28cfa227eb2c0664f6a444",
+    measurementId: "G-MJZ2SSRPG7"
 };
-
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
